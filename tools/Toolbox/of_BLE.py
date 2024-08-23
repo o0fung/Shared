641a49f051a19748d5f353692406911b68c9d06f
o0fung <o0fung@gmail.com>
2024-08-23 14:04:02 +0800

    add Li's IT application

--- tools/Toolbox/of_BLE.py
+++ tools/Toolbox/of_BLE.py
@@ -64,61 +64,75 @@ class BLE:
             # No BLE device is provided
             return
         
-        self.client = BleakClient(self.uuid_dev)
-        await self.client.connect()
+        try:
+            self.client = BleakClient(self.uuid_dev)
+            
+            # Detect attempt to reconnect, then disconnect before connect again
+            if self.client.is_connected:
+                await self.client.disconnect()
+            await self.client.connect()
+            
+        except Exception as e:
+            print(f">> Device {self.name}: Failed to connect to the device UUID: {self.uuid_dev}")
+            print(f">> {e}")
                 
         # Connect to the device
         if not self.client.is_connected:
-            print(f"\n>> Device {self.name}: Failed to connect to the device UUID: {self.uuid_dev}")
             return
         
-        print(f"\n>> Device {self.name}: Connected to UUID: {self.uuid_dev}")
+        print(f">> Device {self.name}: Connected to UUID: {self.uuid_dev}")
+    
+    async def handler_read(self):
+        # Read from characteristic if available
+        if self.uuid_read is None:
+            return
         
+        while True:
+            try:
+                # Start a loop to read values from characteristic if available
+                data = await self.client.read_gatt_char(self.uuid_read)
+                print(f"\n>> Device {self.name}: Read characteristic value from {self.client}: {data.decode('utf-8')}")
+                
+            except Exception as e:
+                print(f"\n>> Device {self.name}: Failed to read characteristic: {e}")
+                
+            await asyncio.sleep(1)
+    
+    async def handler_write(self, data):
+        # Write to characteristic if available
+        if self.uuid_write is None:
+            return
+        
+        await self.client.write_gatt_char(self.uuid_write, data)
+                
+    async def handler_start_notify(self):
+        # Start notification if characteristic available
         if self.uuid_notify is not None:
             # Start a task to receive notification from the characteristic indefinitely if available
             await self.client.start_notify(self.uuid_notify, self.handler_notify)
-            print(f"\n>> Device {self.name}: Subscribed to notifications. Waiting for notifications...")
-            
-            await asyncio.sleep(1)
-            
-        if self.uuid_read is not None:
-            while True:
-                try:
-                    # Start a loop to read values from characteristic if available
-                    data = await self.client.read_gatt_char(self.uuid_read)
-                    self.handler_read(self.client, data)
-                except Exception as e:
-                    print(f"\n>> Device {self.name}: Failed to read characteristic: {e}")
-                    
-                await asyncio.sleep(1)
-                
-    async def disconnect(self):
-        
+            print(f">> Device {self.name}: Started subscribed to notifications. Waiting for notifications...")
+    
+    async def handler_stop_notify(self):
+        # Stop notification if characteristic available
         if self.uuid_notify is not None:
-            # Stop the task to receive notification from the characteristic if available
+            # Start a task to receive notification from the characteristic indefinitely if available
             await self.client.stop_notify(self.uuid_notify)
-        
-        self.handler_stop()     # Anything the program need to do to clean up
+            print(f">> Device {self.name}: Stopped subscribed to notifications...")
             
+    def handler_notify(self, client, data):
+        # Callback function to run when received notification
+        print(f">> Device {self.name}: Received notification value from {self.client}: {data.decode('utf-8')}")
+    
+    async def disconnect(self):
         # Disconnect to the device
         await self.client.disconnect()
+        print(f">> Device {self.name}: Disconnected to UUID: {self.uuid_dev}")
             
-    async def handler_write(self, data):
-        await self.client.write_gatt_char(self.uuid_write, data)
     
-    def handler_read(self, data):
-        print(f"\n>> Device {self.name}: Read characteristic value from {self.client}: {data.decode('utf-8')}")
-        
-    def handler_notify(self, client, data):
-        print(f"\n>> Device {self.name}: Received notification value from {self.client}: {data.decode('utf-8')}")
-    
-    def handler_stop(self):
-        print(f">> Device {self.name}: Disconnect...")
-        
 
 if __name__ == '__main__':
     print(">> Begin...")
 
     ble = BLE()
-    # ble.scan()
-    ble.show_services(uuid='C81B5DAF-543E-D829-55F7-59B0E08C3E6F')
+    ble.scan()
+    # ble.show_services(uuid='')
